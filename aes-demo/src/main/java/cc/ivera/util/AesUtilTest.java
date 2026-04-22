a74e552080cc91d0102247c38605e8c5930449fa
arhi <knife4j@gmail.com>
2026-04-22 11:41:15 +0800

    加密与解密

---
+++ aes-demo/src/main/java/cc/ivera/util/AesUtilTest.java
@@ -0,0 +1,37 @@
+package cc.ivera.util;
+
+public class AesUtilTest {
+
+    // 测试用例
+    public static void main(String[] args) {
+        try {
+            // 生成密钥
+            String key256 = AesUtil.generateKey(256);
+            System.out.printf("key:%s\n",key256);
+
+            // 测试数据
+            String testData1 = "Hello world!";
+
+            // 256-bit测试
+            String encrypted256 = AesUtil.encrypt(testData1, key256);
+            String decrypted256 = AesUtil.decrypt(encrypted256, key256);
+
+
+            String data = AesUtil.encrypt(testData1, key256);
+
+            System.out.println(encrypted256);
+            System.out.println(data);
+
+
+            System.out.println("256-bit Test:");
+            System.out.println("Key: " + key256);
+            System.out.println("Original: " + testData1);
+            System.out.println("Encrypted: " + encrypted256);
+            System.out.println("Decrypted: " + decrypted256);
+            System.out.println("Match: " + testData1.equals(decrypted256));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ aes-demo/src/main/java/cc/ivera/util/Demo02.java
@@ -0,0 +1,42 @@
+/**
+ * 2023年4月11日上午11:09:02
+ */
+package cc.ivera.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @author XWF
+ */
+public class Demo02 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String pass = "1122334455667788";
+        String msg = """
+                {"infoList":[{"carplot_id":"1491010","carplot_type":0,"equip_id":"PELSL0172","equip_time":"1625488943","plate":"京A12345","carplot_pics":[],"status":1},{"carplot_id":"1491013","carplot_type":1,"equip_id":"PELSL0172","equip_time":"1625488943","carplot_pics":[],"status":"0"},{"carplot_id":"1491014","carplot_type":0,"equip_id":"PELSL0172","equip_time":"1625488943","status":-1}],"park_id":"p001"}
+                """;
+        try {
+            byte[] key = pass.getBytes("utf-8");
+            SecretKeySpec keyspec = new SecretKeySpec(key, "AES");
+            //加密
+            Cipher cipher = Cipher.getInstance("AES/OFB24/PKCS5Padding");
+            IvParameterSpec iv = new IvParameterSpec("1234567887654321".getBytes("utf-8"));
+            cipher.init(Cipher.ENCRYPT_MODE, keyspec, iv);
+            byte[] encodeBytes = cipher.doFinal(msg.getBytes("utf-8"));
+            System.out.println("使用AES_OFB加密后：" + new String(encodeBytes));
+
+            //解密
+            cipher.init(Cipher.DECRYPT_MODE, keyspec, iv);
+            byte[] decodeBytes = cipher.doFinal(encodeBytes);
+            System.out.println("使用AES_OFB解密后：" + new String(decodeBytes, "utf-8"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
