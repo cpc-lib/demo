1fc648e428d3791202ab2b991bcb81a375a2dd52
cpc-lib <knife4j@gmail.com>
2026-06-14 12:55:07 +0800

    remove files under springboot-payment-v1

--- springboot-payment-v1/payment-demo-go-dm/payment-demo-go/internal/service/refund_status_dialect_test.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "testing"
-
-func TestRefundAmountSumExpression_CurrentBehaviorUsesPortableSQL(t *testing.T) {
-	if refundAmountSumExpression != "COALESCE(sum(refund),0)" {
-		t.Fatalf("refundAmountSumExpression = %q, want COALESCE(sum(refund),0)", refundAmountSumExpression)
-	}
-}
--- springboot-payment-v1/payment-demo-go-dm/payment-demo-vue/src/views/Success.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="bg-fa of" style="padding: 40px;">
-    <el-alert
-        title="支付成功！"
-        type="success">
-    </el-alert>
-  </div>
-</template>
